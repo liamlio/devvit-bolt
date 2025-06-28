4bcea8d5f03d0b4670085f1e557e1ae40e71ab59
Liam <35613341+liamlio@users.noreply.github.com>
2025-06-28 15:01:16 -0400

    Updated CreateGameInterface.tsx

--- src/devvit/components/CreateGameInterface.tsx
+++ src/devvit/components/CreateGameInterface.tsx
@@ -64,9 +64,10 @@ export const CreateGameInterface = ({
   };
 
   return (
+    <CarnivalBackground>
     <vstack width="100%" height="100%" alignment="center middle" padding="large">
       <CarnivalCard>
-        <text size="xlarge" alignment="center" color={CarnivalTheme.colors.text}>🎪 Create Your Game</text>
+        <text size="xxlarge" alignment="center" color={CarnivalTheme.colors.text}>🎪 Create Your Game</text>
         <text alignment="center" color={CarnivalTheme.colors.text}>
           Ready to create your Two Truths One Lie game? Use our enhanced form with real-time character counting!
         </text>
@@ -110,5 +111,6 @@ export const CreateGameInterface = ({
         </hstack>
       </CarnivalCard>
     </vstack>
+    </CarnivalBackground>CarnivalBackground>
   );
 };
