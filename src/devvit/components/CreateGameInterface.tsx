2d4e19714a148cbbd8383bc76cd6c4230849b2b7
Liam <35613341+liamlio@users.noreply.github.com>
2025-06-28 21:24:29 -0400

    Updated CreateGameInterface.tsx

--- src/devvit/components/CreateGameInterface.tsx
+++ src/devvit/components/CreateGameInterface.tsx
@@ -201,7 +201,7 @@ export const CreateGameInterface = ({
               >
                 Create Post 🎪
               </button>
-                            <button
+               <button
                 appearance="secondary"
                 onPress={onBack}
                 width="100%"
